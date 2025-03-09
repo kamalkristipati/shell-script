a03fd321a97cdc59681c9076eb3e9636a9cfa522
kamalkristipati <kamalkristipati82@gmail.com>
2025-03-09 16:15:11 +1100

    kkk

--- arrays.sh
+++ arrays.sh
@@ -2,7 +2,7 @@
 
 NAMES=(kamal,kiran,pavan)
 
-echo "First name is $[NAMES{0}]"
-echo "Second name is $[NAMES{1}]"
-echo "Third name is $[Nmaes{2}]"
-echo "All Names $[NAMES{@}]"
+echo "First name is${NAMES[0]}"
+echo "second name is ${NAMES[1]}"
+echo "THird name is ${NAMES[2}"
+echo "ALL the names $NAMES[@]}"
