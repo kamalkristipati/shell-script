48e6c60e0857189fa5545f41f1206d91a8b4cb94
kamalkristipati <kamalkristipati82@gmail.com>
2025-03-09 16:03:15 +1100

    scripsts

---
+++ addition.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "Eneter first number"
+read N1
+
+echo "Eneter second number"
+read N2
+
+N3=$[N1+N2]
+
+echo " The addition of two numbers is $N3"
---
+++ arrays.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+NAMES=(kamal,kiran,pavan)
+
+echo "First name is $[NAMES{0}]"
+echo "Second name is $[NAMES{1}]"
+echo "Third name is $[Nmaes{2}]"
+echo "All Names $[NAMES{@}]"
