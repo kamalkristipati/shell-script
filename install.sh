93b65fc82f44a27cf27b0ea6d8de0aa56a7821a3
kamalkristipati <kamalkristipati82@gmail.com>
2025-03-10 14:27:36 +1100

    install

--- install.sh
+++ install.sh
@@ -7,5 +7,22 @@ then
    echo " You must have root access to install"
    exit 1
 fi
-dnf install mysql -y
+dnf install mysqll -y
 
+if [ $? -ne 0 ]
+then
+    echo "Failure in stallation"
+    exit 1
+else 
+    echo "installation success"
+fi 
+
+dnf install git -y 
+
+if [ $? -ne 0 ]
+then
+    echo "installation failed"
+    exit 1
+else
+    echo "installation success
+fi 
